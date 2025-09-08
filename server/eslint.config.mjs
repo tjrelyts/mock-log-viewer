109dcb90bc2c1214a859862db5f5c53621e39af4
Tyler Santosuosso <tylerj-santo@outlook.com>
2025-09-07 23:41:24 -0400

    Working LINT commit

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -4,6 +4,15 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.node, ...globals.browser } },
+    ignores: ["node_modules/**", "dist/**"],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
   pluginReact.configs.flat.recommended,
 ]);
